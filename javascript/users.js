69419e6db3b19b39933d25b87ee8a48f28682d04
Surya Kiran <surya@hifx.co.in>
2024-06-12 15:39:25 +0530

    login backend completed

--- javascript/users.js
+++ javascript/users.js
@@ -1,8 +1,26 @@
 const searchBar = document.querySelector(".users .search input"),
-searchBtn = document.querySelector(".users .search button");
+searchBtn = document.querySelector(".users .search button"),
+usersList = document.querySelector(".users .users-list");
 
 searchBtn.onclick = () => {
     searchBar.classList.toggle("active");
     searchBar.focus();
     searchBtn.classList.toggle("active");
 }
+
+setInterval(() => {
+    // starting Ajax
+    let xhr = new XMLHttpRequest(); // creating XML object
+    xhr.open("GET", "php/users.php");
+    xhr.onload = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let data = xhr.response;
+                usersList.innerHTML = data;
+            }
+        }
+    }
+    // sending the form data to php through ajax
+    xhr.send(); // sending the form data
+
+}, 500);
--- users.php
+++ users.php
@@ -1,24 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Chat App</title>
-</head>
+<?php
+
+    include_once "php/config.php";
+
+    session_start();
+    if (!isset($_SESSION['unique_id'])) {
+        header("location: login.php");
+    }
+?>
+
+<?php include_once "header.php"; ?>
+
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
+                <?php
+                    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+                    if (mysqli_num_rows($sql) > 0) {
+                        $row = mysqli_fetch_assoc($sql);
+                    }
+                ?>
                 <div class="content">
-                    <img src="images/img.webp" alt="">
+                    <img src="php/user-images/<?php echo $row['img']; ?>" alt="">
                     <div class="details">
-                        <span>Surya Kiran</span>
-                        <p>Active now</p>
+                        <span><?php echo $row['fname'] . " " . $row['lname']; ?></span>
+                        <p><?= $row['status']; ?></p>
                     </div>
                 </div>
                 <a href="#" class="logout">Logout</a>
@@ -29,76 +35,7 @@
                 <button><i class="fas fa-search"></i></button>
             </div>
             <div class="users-list">
-                <a href="#">
-                    <div class="content">
-                        <img src="images/img.webp">
-                        <div class="details">
-                            <span>Surya Kiran</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="images/img.webp">
-                        <div class="details">
-                            <span>Surya Kiran</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="images/img.webp">
-                        <div class="details">
-                            <span>Surya Kiran</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="images/img.webp">
-                        <div class="details">
-                            <span>Surya Kiran</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="images/img.webp">
-                        <div class="details">
-                            <span>Surya Kiran</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="images/img.webp">
-                        <div class="details">
-                            <span>Surya Kiran</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="images/img.webp">
-                        <div class="details">
-                            <span>Surya Kiran</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
+                
             </div>
         </section>
     </div>
