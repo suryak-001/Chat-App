ec593e114254fae6f858add830897b026b0726ba
Surya Kiran <surya@hifx.co.in>
2024-06-13 14:08:39 +0530

    Updated README

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Chat-App
 
-Developed a real time chat app
+## Overview
+
+Developed a real-time chat application using HTML, CSS, JavaScript, and PHP. The application allows users to engage in live conversations with other users.
+
+## Technologies Used
+
+- HTML: Used for structuring the layout and content of the application.
+- CSS: Used for styling the application and creating a visually appealing user interface.
+- JavaScript: Used for adding interactivity to the application, such as real-time updates and user input validation.
+- PHP: Used for server-side logic, such as handling user authentication and database operations.
+
+## Key Features
+
+- Real-time chat functionality: Users can send and receive messages in real-time, allowing for instant communication.
+- User authentication: Users can create accounts and log in to access the chat application.
+- User profiles: Users can view and edit their profiles, including their profile picture and bio.
--- javascript/signup.js
+++ javascript/signup.js
@@ -6,24 +6,39 @@ form.onsubmit = (e) => {
     e.preventDefault(); // preventing form from submitting
 }
 
+/**
+ * Handle the click event of the continue button.
+ * Sends an AJAX POST request to the signup.php file with form data.
+ * If the request is successful and the response is 'success',
+ * redirects the user to the 'users.php' page.
+ * Otherwise, displays the response text in the error container.
+ */
 continueBtn.onclick = () => {
     // starting Ajax
     let xhr = new XMLHttpRequest(); // creating XML object
+
+    // Opening an asynchronous POST request to the signup.php file
     xhr.open("POST", "php/signup.php");
+
+    // Handling the response of the request
     xhr.onload = () => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 let data = xhr.response;
+
+                // If the response is 'success', redirect to the 'users.php' page
                 if (data === "success") {
                     location.href = "users.php";
-                } else {
+                } 
+                // Otherwise, display the response text in the error container
+                else {
                     errorText.textContent = data;
                     errorText.style.display = "block";
                 }
             }
         }
     }
-    // sending the form data to php through ajax
+
     let formData = new FormData(form)   // creating new formData object
     xhr.send(formData); // sending the form data
 }
